738cd3d94b191ce86fdc6d78c0abd835420908b7
hyl <hanyongle@zgzb.com>
2024-08-28 15:42:03 +0800

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core'
 import { ChildrenOutletContexts, RouterLink, RouterModule, RouterOutlet } from '@angular/router'
 import { menusList } from './common/menu-data'
-import { slideInAnimation } from './animation/slideInAnimation';
+import { slideInAnimation } from './animation/slideInAnimation'
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, RouterLink],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    RouterLink,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  animations:[slideInAnimation]
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   menusList = menusList
 
   constructor(private contexts: ChildrenOutletContexts) {}
   getRouteAnimationData() {
-    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation']
+  }
+
+  swChange(e: MatSlideToggleChange) {
+    console.log(e)
+  }
+
+  setThem(e:number){
+    console.log(e);
+    
   }
 }
