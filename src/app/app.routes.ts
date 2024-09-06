fcbb202d15d03f9f4f600139675957a522504ff1
hyl <hanyongle@zgzb.com>
2024-09-06 15:16:11 +0800

    --

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,44 @@
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './pages/home/home.component'
-import { SocialComponent } from './pages/social/social.component'
+// import { SocialComponent } from './pages/social/social.component'
+// import { LiveComponent } from './pages/live/live.component'
+// import { SignComponent } from './pages/live/sign/sign.component'
+// import { PkComponent } from './pages/live/pk/pk.component'
+// import { PerformanceComponent } from './pages/performance/performance.component'
+// import { OrderComponent } from './pages/order/order.component'
+// import { AbnormalComponent } from './pages/order/abnormal/abnormal.component'
+
 import { NotfoundComponent } from './pages/notfound/notfound.component'
-import { LiveComponent } from './pages/live/live.component'
-import { SignComponent } from './pages/live/sign/sign.component'
-import { PkComponent } from './pages/live/pk/pk.component'
-import { PerformanceComponent } from './pages/performance/performance.component'
-import { OrderComponent } from './pages/order/order.component'
-import { AbnormalComponent } from './pages/order/abnormal/abnormal.component'
 import { loadingGuard } from './guards/loading.guard'
+
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
     title: '首页',
     data: { animation: 'HomePage' },
-    canActivate:[loadingGuard]
+    canActivate: [loadingGuard]
   },
   {
     path: 'live',
-    component: LiveComponent,
+    // component: LiveComponent,
+    loadComponent: () => import('./pages/live/live.component').then((m) => m.LiveComponent),
+
     title: '直播管理',
     children: [
       { path: '', redirectTo: 'sign', pathMatch: 'full' },
       {
         path: 'sign',
-        component: SignComponent,
+        // component: SignComponent,
+        loadComponent:()=>import('./pages/live/sign/sign.component').then(m=>m.SignComponent),
         title: '直播签到统计',
         data: { animation: 'SignPage' }
       },
       {
         path: 'pk',
-        component: PkComponent,
+        // component: PkComponent,
+        loadComponent:()=>import('./pages/live/pk/pk.component').then(m=>m.PkComponent),
         title: '直播pk',
         data: { animation: 'PkPage' }
       }
@@ -40,20 +46,23 @@ export const routes: Routes = [
   },
   {
     path: 'performance',
-    component: PerformanceComponent,
+    // component: PerformanceComponent,
+    loadComponent: () => import('./pages/performance/performance.component').then((m) => m.PerformanceComponent),
     title: '业绩统计',
     data: { animation: 'PerformancePage' },
-    canActivate:[loadingGuard]
+    canActivate: [loadingGuard]
   },
   {
     path: 'order',
-    component: OrderComponent,
+    // component: OrderComponent,
+    loadComponent: () => import('./pages/order/order.component').then((m) => m.OrderComponent),
     title: '订单管理',
     children: [
       { path: '', redirectTo: 'abnormal', pathMatch: 'full' },
       {
         path: 'abnormal',
-        component: AbnormalComponent,
+        // component: AbnormalComponent,
+        loadComponent:()=>import('./pages/order/abnormal/abnormal.component').then(m=>m.AbnormalComponent),
         title: '异常开单',
         data: { animation: 'AbnormalPage' }
       }
@@ -61,7 +70,8 @@ export const routes: Routes = [
   },
   {
     path: 'social',
-    component: SocialComponent,
+    // component: SocialComponent,
+    loadComponent: () => import('./pages/social/social.component').then((m) => m.SocialComponent),
     title: '社交',
     data: { animation: 'SocialPage' }
   },
