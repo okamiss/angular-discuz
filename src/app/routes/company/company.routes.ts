d189e88764177638203dc626a5d9d4eae8b6bd28
hyl <hanyongle@zgzb.com>
2024-09-12 13:17:16 +0800

    --

--- src/app/routes/company/company.routes.ts
+++ src/app/routes/company/company.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router'
 import { AssetComponent } from './asset/asset.component'
+import { StaffComponent } from './staff/staff.component'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'asset', pathMatch: 'full' },
-  { path: 'asset', component: AssetComponent }
+  { path: 'asset', component: AssetComponent },
+  { path: 'staff', component: StaffComponent }
 ]
